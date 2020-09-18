6e32a5958dfa915c76f202023fc6d6fb24fec012
Joe DCruz <joedcruz99@gmail.com>
2020-09-18 18:39:07 +0400

    Adding to azure

--- server.js
+++ server.js
@@ -6,21 +6,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/hello-world'));
 
-// app.all('*'),(req, res) => {
-//     res.status(200).sendFile(__dirname + '/dist/hello-world/index.html');
-// }
-
 app.get('/*', function(req,res) {    
     res.sendFile(path.join(__dirname + '/dist/hello-world/index.html'));
 });
-
-// app.all('/*', function(req, res, next) {
-//     res.sendFile('index.html', { root: __dirname});
-// });
-
-// app.get('/*', function(req,res) {    
-//     res.sendFile(__dirname + '/dist/hello-world/index.html');
-// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
