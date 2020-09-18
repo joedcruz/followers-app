85f85521cd623dedd58417fe9fc0461ca4bf5fbe
Joe DCruz <joedcruz99@gmail.com>
2020-09-18 16:44:41 +0400

    Prepare4 for Heroku

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/hello-world'));
 
-app.all('*'),(req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/hello-world/index.html');
-}
+// app.all('*'),(req, res) => {
+//     res.status(200).sendFile(__dirname + '/dist/hello-world/index.html');
+// }
 
-// app.get('/*', function(req,res) {    
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
+app.get('/*', function(req,res) {    
+    res.sendFile(path.join(__dirname+'/dist/hello-world/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
